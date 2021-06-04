8bd552fb89f6f8077d4e56620a85f2df2d87d8fa
hodinhhung <85335668+hodinhhung@users.noreply.github.com>
2021-06-04 17:14:09 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'chị Hiền à!',
+    introTitle: 'bà lương lươn lẹo ak!',
     introDesc: `Trái đất vốn lạ thường
-    để rồi lạc vào tim em thế này😁
+    để rồi lạc vào tim tao thế này😁
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
+    btnYes: 'yêu tao nhé <33',
     btnNo: 'Không, em zai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
-    btnReply: 'Gửi cho Đăng <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'em biết mà 🥰. Yêu chị Hiền nhiều nhiều <I love 3000> 😘😘',
-    messDesc: 'hết dịch em rủ chị đi chơi nhé.'
+    question:'Trên thế giới hơn 7 tỉ người mà sao mi lùn thế <3',
+    btnReply: 'Gửi cho Hưng <3',
+    reply: 't yêu m từ lâu rồi',
+    mess: 't biết lâu rồi mà',
+    messDesc: 'nhưg t ko thích m.'
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100068494689461
